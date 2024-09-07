9e5a0f20274997a3df8cf21c6c1611013bdf1efd
dedeheri <dedeherii60@gmail.com>
2024-09-07 18:47:20 +0700

    add prisma orm

---
+++ src/app/api/v1/video/route.ts
@@ -0,0 +1,61 @@
+import { prisma } from "@/lib/prisma.db";
+import { response } from "@/lib/response";
+
+export const POST = async (req: Request) => {
+  try {
+    const data = await req.json();
+
+    await prisma.video.createMany({
+      data: data,
+      skipDuplicates: true,
+    });
+
+    return response({ message: "Success", status: 200 });
+  } catch (error) {
+    return response({
+      message: "error",
+      error: "Something went wrong",
+      status: 500,
+    });
+  }
+};
+
+export const GET = async (req: Request) => {
+  try {
+    const { searchParams } = new URL(req.url);
+
+    const filter = searchParams.get("filter") as string;
+
+    const result = await prisma.video.findMany({
+      include: {
+        channel: true,
+      },
+    });
+
+    const filterResult = result.filter((data) => {
+      return data?.channel?.name?.toLowerCase().includes(filter.toLowerCase());
+    });
+
+    if (filter === "All") {
+      return response({
+        message: "Success",
+        success: true,
+        data: result,
+        status: 200,
+      });
+    } else {
+      return response({
+        message: "Success",
+        success: true,
+        data: filterResult,
+        status: 200,
+      });
+    }
+  } catch (error) {
+    return response({
+      message: "error",
+      error: "Something went wrong",
+      status: 500,
+    });
+  }
+};
